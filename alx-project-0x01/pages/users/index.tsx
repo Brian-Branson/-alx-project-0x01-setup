04fdf2f3ee61b1faaca4fcf276fbc3d13140c264
Brian <karanjabrian382@gmail.com>
2025-05-27 23:30:57 +0300

    complete

--- alx-project-0x01/pages/users/index.tsx
+++ alx-project-0x01/pages/users/index.tsx
@@ -1,6 +1,9 @@
+import Header from "@/components/layout/Header";
+
 const homepage = () => {
   return (
     <div>
+      <Header/>
       <h1>Welcome to the User Homepage</h1>
       <p>This is the main page for user-related content.</p>
     </div>
